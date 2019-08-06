269197018d65faefaa4b344edf56ad2fef38af86
Nicole Sigei <v-nisige@microsoft.com>
2019-08-06 11:39:27 +0300

    Getting a site resource.
    Retrieving properties for a site resource. For example : getting a collection of lists and  items in a list and creating , updating and deleting items in a list.

---
+++ AssetTracking/AssetTracking/Models/ISites.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetTracking.Models
+{
+    interface ISites
+    {
+        Task<ISiteListsCollectionPage> GetLists(GraphServiceClient graphClient, string siteId);
+        Task<IListItemsCollectionPage> GetListItems(GraphServiceClient graphServiceClient, string siteId, string listId);
+        Task<bool> AddListItem(GraphServiceClient graphServiceClient, string siteId, string listId, IDictionary<string, object> data);
+        Task<bool> UpdateListItem(GraphServiceClient graphServiceClient, string siteId, string listId, string itemId, IDictionary<string, object> data);
+        Task<bool> DeleteListItem(GraphServiceClient graphServiceClient, string siteId, string listId, string itemId);
+    }
+}
