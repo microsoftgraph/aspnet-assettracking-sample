874ff09721665fa3805f17bf1f506f149385d530
Nicole Sigei <v-nisige@microsoft.com>
2019-08-08 12:44:13 +0300

    Adding Office Book Model class and Office Book Controller
    The Office Book Controller contains functions that perform the CRUD operations that is adding, updating and deleting items from our Office Book SharePoint list and also getting items from the list.

--- AssetTracking/AssetTracking/Controllers/OfficeBooksController.cs
+++ AssetTracking/AssetTracking/Controllers/OfficeBooksController.cs
@@ -13,18 +13,20 @@ using Newtonsoft.Json;
 
 namespace AssetTracking.Controllers
 {
+    [Route("[controller]")]
     public class OfficeBooksController : Controller
     {
         private readonly IConfiguration _configuration;
         private readonly IHostingEnvironment _env;
         private readonly IGraphSdkHelper _graphSdkHelper;
+
         private readonly string siteId = "m365b267815.sharepoint.com,6e1261a1-6d03-432a-95c0-e1c7705aef5f,f43d258c-ece0-476a-a1c0-018d359817d5";
         private readonly string listId;
         private ISiteListsCollectionPage sharePointLists;
-        private readonly string itemId = null;
+        private string itemId = null;
         private readonly string title;
         private readonly string ISBN;
-        List<OfficeBooks> _officeBooksDirectoryList = new List<OfficeBooks>();
+        List<OfficeBook> _officeBooksDirectoryList = new List<OfficeBook>();
 
         public OfficeBooksController(IConfiguration configuration, IHostingEnvironment hostingEnvironment, IGraphSdkHelper graphSdkHelper)
         {
@@ -32,53 +34,74 @@ namespace AssetTracking.Controllers
             _env = hostingEnvironment;
             _graphSdkHelper = graphSdkHelper;
         }
-        public GraphServiceClient graphserviceClient { get; private set; }
-        [HttpGet]
-        public async Task<IActionResult> OfficeBooks()
+        public GraphServiceClient GraphClient { get; private set; }
+
+        [Route("")]
+        public IActionResult OfficeBooks()
         {
             if (User.Identity.IsAuthenticated)
             {
-                graphserviceClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
-                var user = graphserviceClient.Me.Request().GetAsync();
 
-                sharePointLists = await Sites.GetLists(graphserviceClient, siteId);
-                if (sharePointLists != null)
-                {
-                    var _officeBookList = sharePointLists.Where(x => x.DisplayName.Contains("Books")).FirstOrDefault();
-                    var _listId = _officeBookList.Id;
-                    var _officeBookItems = await Sites.GetListItems(graphserviceClient, siteId, _listId);
+                GraphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                var user = GraphClient.Me.Request().GetAsync();
 
 
-                    foreach (var item in _officeBookItems)
-                    {
-                        var resourceList = item.Fields.AdditionalData;
-                        var jsonString = JsonConvert.SerializeObject(resourceList);
-
-                        var officeResource = JsonConvert.DeserializeObject<OfficeBooks>(jsonString);
-                        _officeBooksDirectoryList.Add(officeResource);
-                    }
-                }
-                    ViewBag.List = _officeBooksDirectoryList;
             }
             return View("~/Views/Admin/OfficeBooks.cshtml");
         }
 
+        [Route("/GetBooks")]
         [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> AddBook(OfficeBooks officeBook)
+        private async Task<IActionResult> GetBooks()
         {
-            graphserviceClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+            sharePointLists = await Sites.GetLists(GraphClient, siteId);
+            if (sharePointLists != null)
+            {
+                var _officeBookList = sharePointLists.Where(x => x.DisplayName.Contains("Office Books")).FirstOrDefault();
+                var _listId = _officeBookList.Id;
+                var _officeBookItems = await GetListItems(GraphClient, siteId, _listId);
 
-            sharePointLists = await Sites.GetLists(graphserviceClient, siteId);
+                List<OfficeBook> _officeBooksDirectoryList = new List<OfficeBook>();
+
+                foreach (var item in _officeBookItems)
+                {
+                    var resourceList = item.Fields.AdditionalData;
+                    var jsonString = JsonConvert.SerializeObject(resourceList);
+
+                    var officeResource = JsonConvert.DeserializeObject<OfficeBook>(jsonString);
+                    officeResource.SharePointItemId = item.Id;
+                    _officeBooksDirectoryList.Add(officeResource);
+                }
+
+                ViewBag.List = _officeBooksDirectoryList;
+            }
+            return View("~/Views/Shared/Error.cshtml");
+        }
+        private async static Task<IListItemsCollectionPage> GetListItems(GraphServiceClient graphClient, string siteId, string listId)
+        {
+            IListItemsCollectionPage listItems = await Sites.GetListItems(graphClient, siteId, listId);
+            return listItems;
+        }
+
+        [Route("/AddBooks")]
+        [HttpPost]
+        [AutoValidateAntiforgeryTokenAttribute]
+        public async Task<IActionResult> AddBooks(OfficeBook officeBook)
+        {
+            GraphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+
+            sharePointLists = await Sites.GetLists(GraphClient, siteId);
 
             if (sharePointLists != null)
             {
                 var addbook = sharePointLists.Where(b => b.DisplayName.Contains("Books")).FirstOrDefault();
                 string _listId = addbook.Id;
-                officeBook.BookId = Guid.NewGuid();
+                officeBook.BookID = Guid.NewGuid();
+
                 IDictionary<string, object> data = new Dictionary<string, object>
                 {
-                    { "BookID", officeBook.BookId },
+
+                    { "BookID", officeBook.BookID },
                     { "Resource_x0020_IDLookupId", "2" },
                     { "Title", officeBook.Title },
                     { "ISBN", officeBook.ISBN },
@@ -86,38 +109,111 @@ namespace AssetTracking.Controllers
                     { "BookTitle", officeBook.BookDescription }
                 };
 
-                bool result = await Sites.AddListItem(graphserviceClient, siteId,
+                bool result = await Sites.AddListItem(GraphClient, siteId,
                                                       _listId,
                                                       data);
                 if (result)
                 {
-                    return await OfficeBooks();
+                    return RedirectToAction("GetBooks");
                 }
             }
             return View("~/Views/Shared/Error.cshtml");
         }
-        private async Task UpdateBook(string ItemId)
+
+        [HttpPut]
+        public RedirectToActionResult Update(OfficeBook officeBook)
         {
-            IDictionary<string, object> data = new Dictionary<string, object>();
+            GraphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
 
-            string listItemId = itemId;
+            sharePointLists = Sites.GetLists(GraphClient, siteId).GetAwaiter().GetResult();
 
-            var officeBookItem = _officeBooksDirectoryList.Where(b => b.SharePointItemId.Contains(itemId)).FirstOrDefault();
-            officeBookItem.Title = title;
-
-            var jsonString = JsonConvert.SerializeObject(officeBookItem);
-            data = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
-
-            bool result = await Sites.UpdateListItem(graphserviceClient,siteId,listId, ItemId, data);
-
-            if (result)
+            if (sharePointLists != null)
             {
-                
-                //GetBooks();
+                var addbook = sharePointLists.Where(b => b.DisplayName.Contains("Books")).FirstOrDefault();
+                string _listId = addbook.Id;
+
+                IDictionary<string, object> data = new Dictionary<string, object>
+                {
+
+                    { "BookID", officeBook.BookID },
+                    { "Resource_x0020_IDLookupId", "2" },
+                    { "Title", officeBook.Title },
+                    { "ISBN", officeBook.ISBN },
+                    { "Author0", officeBook.Author },
+                    { "BookTitle", officeBook.BookDescription }
+                };
+
+                bool result = Sites.UpdateListItem(GraphClient, siteId,
+                                                      _listId, itemId,
+                                                      data).GetAwaiter().GetResult();
+                if (result)
+                {
+                    return RedirectToAction("GetBooks");
+                }
             }
+            return RedirectToAction("OfficeBooks");
         }
 
-    }
+        [Route("/UpdateBookDetails")]
+        [HttpPost]
+        [AutoValidateAntiforgeryTokenAttribute]
+        public async Task<IActionResult> UpdateBookDetails([FromForm]OfficeBook officeBook)
+        {
+            GraphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
 
-    
+            sharePointLists = await Sites.GetLists(GraphClient, siteId);
+            string userItemId = officeBook.SharePointItemId;
+
+            if (sharePointLists != null)
+            {
+                var addbook = sharePointLists.Where(b => b.DisplayName.Contains("Office Books")).FirstOrDefault();
+                string _listId = addbook.Id;
+
+                string itemId = userItemId;
+
+                IDictionary<string, object> data = new Dictionary<string, object>
+                {
+                    { "Title", officeBook.Title },
+                    { "ISBN", officeBook.ISBN },
+                    { "Author0", officeBook.Author },
+                    { "BookTitle", officeBook.BookDescription }
+                };
+
+                bool result = await Sites.UpdateListItem(GraphClient, siteId,
+                                                      _listId, itemId,
+                                                      data);
+                if (result)
+                {
+                    return RedirectToAction("GetBooks");
+                }
+            }
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        [Route("/DeleteBook")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteBook(OfficeBook officeBook)
+        {
+            GraphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+
+            sharePointLists = await Sites.GetLists(GraphClient, siteId);
+            string userItemId = officeBook.SharePointItemId;
+
+            if (sharePointLists != null)
+            {
+                var addbook = sharePointLists.Where(b => b.DisplayName.Contains("Office Books")).FirstOrDefault();
+                string _listId = addbook.Id;
+
+                string itemId = userItemId;
+
+                bool result = await Sites.DeleteListItem(GraphClient, siteId,
+                                                      _listId, itemId);
+                if (result)
+                {
+                    return RedirectToAction("GetBooks");
+                }
+            }
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
 }
---
+++ AssetTracking/AssetTracking/Models/OfficeBook.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace AssetTracking.Models
+{
+    public class OfficeBook
+    {
+        [JsonIgnore]
+        public string SharePointItemId { get; set; }
+
+        [JsonProperty(PropertyName = "BookID")]
+
+        public Guid BookID { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "Resource_x0020_IDLookupId")]
+        public string ResourceId { get; set; }
+
+        public string ISBN { get; set; }
+
+        [JsonProperty(PropertyName = "BookTitle")]
+        public string BookDescription { get; set; }
+
+        [JsonProperty(PropertyName = "Author0")]
+        public string Author { get; set; }
+    }
+}
