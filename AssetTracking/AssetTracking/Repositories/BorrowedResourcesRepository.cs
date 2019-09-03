e7892b4898fb1d004dab1965e0cc2a57b0ca75a2
Nicole Sigei <v-nisige@microsoft.com>
2019-09-03 13:18:21 +0300

    Introduced Borrowed Resources

---
+++ AssetTracking/AssetTracking/Repositories/BorrowedResourcesRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetTracking.Repositories
+{
+    public class BorrowedResourcesRepository
+    {
+    }
+}
