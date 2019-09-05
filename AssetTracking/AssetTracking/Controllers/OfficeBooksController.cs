0e769898e374aafd913aa87db08fa556229ed425
Nicole Sigei <v-nisige@microsoft.com>
2019-09-05 11:34:07 +0300

    Resolved merge conflicts

--- AssetTracking/AssetTracking/Controllers/OfficeBooksController.cs
+++ AssetTracking/AssetTracking/Controllers/OfficeBooksController.cs
@@ -24,5 +24,80 @@ namespace AssetTracking.Controllers
             _officeBookRepository = officeBookRepository;
             siteId = configuration["SiteId"];
         }
+
+        public ActionResult OfficeBooks()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> OfficeBooksGet()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                List<OfficeBook> officeBook = await _officeBookRepository.GetBooks(_graphClient, siteId);
+
+                return Json(new { data = officeBook });
+            }
+            else
+            {
+                return Json(new { IsSuccess = false });
+            }
+        }
+
+        public async Task<ActionResult> OfficeBooksGetbyId(string Id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                List<OfficeBook> officeBookList = await _officeBookRepository.GetBooks(_graphClient, siteId);
+                OfficeBook officeBook = officeBookList.Where(d => d.ItemId == Id).FirstOrDefault();
+                return Json(officeBook);
+            }
+            return Json(null);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddBook(OfficeBook officeBook)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                bool result = await _officeBookRepository.AddBook(officeBook, _graphClient, siteId);
+                return Json(new { IsSuccess = result });
+            }
+            else
+            {
+                return Json(new { IsSuccess = false });
+            }
+        }
+
+        public async Task<JsonResult> UpdateBook(OfficeBook officeBook)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                bool result = await _officeBookRepository.UpdateBook(officeBook, _graphClient, siteId);
+                return Json(new { IsSuccess = result });
+            }
+            else
+            {
+                return Json(new { IsSuccess = false });
+            }
+        }
+
+        public async Task<JsonResult> DeleteBook(OfficeBook officeBook)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+                bool result = await _officeBookRepository.DeleteBook(officeBook, _graphClient, siteId);
+                return Json(new { IsSuccess = result });
+            }
+            else
+            {
+                return Json(new { IsSuccess = false });
+            }
+        }
     }
 }
