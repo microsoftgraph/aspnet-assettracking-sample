6b80b2900d7147a0fbca952d0e00f3a0f8e823b7
Nicole Sigei <v-nisige@microsoft.com>
2019-09-09 12:27:12 +0300

    authenticated User's side

--- AssetTracking/AssetTracking/Controllers/AdminController.cs
+++ AssetTracking/AssetTracking/Controllers/AdminController.cs
@@ -2,12 +2,27 @@
 using Microsoft.AspNetCore.Mvc;
 using AssetTracking.Models;
 using Microsoft.AspNetCore.Authorization;
+using AssetTracking.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.AspNetCore.Hosting;
+using System.Security.Claims;
 
 
 namespace AssetTracking.Controllers
 { 
-    public class AdminController : Controller
-    {
+   public class AdminController : Controller
+   {
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _env;
+        private readonly IGraphSdkHelper _graphSdkHelper;
+        public AdminController(IConfiguration configuration, IHostingEnvironment hostingEnvironment, IGraphSdkHelper graphSdkHelper)
+        {
+            _configuration = configuration;
+            _env = hostingEnvironment;
+            _graphSdkHelper = graphSdkHelper;
+        }
+        public GraphServiceClient GraphClient { get; private set; }
         [Authorize]
         public IActionResult Index() { 
             return View();
